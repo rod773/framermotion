d174d3000630afe3464e38b91af306f04d03bc6f
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-25 17:29:10 +0100

    images array

---
+++ src/Images.js
@@ -0,0 +1,8 @@
+import image1 from "./images/1.jpg";
+import image2 from "./images/2.jpg";
+import image3 from "./images/3.jpg";
+import image4 from "./images/4.jpg";
+
+const images = [image1, image2, image3, image4];
+
+export default images;
